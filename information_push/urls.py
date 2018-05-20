c4cffa9829176a41d299f00281ed7f95f8307c5f
sunguangyong <self_brave@163.com>
2018-05-20 22:16:07 +0800

    调用接口

--- information_push/urls.py
+++ information_push/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^receive_information/', include("receive_information.urls")),
+    url(r'^equipment/', include("equipment.urls")),
 ]
